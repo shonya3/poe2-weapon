bb121edcc43725f1a557136edc2cc1e49ef64c33
shonya3 <poeshonya3@gmail.com>
2025-01-19 08:13:29 +0300

    write scraped base into crates/parser/bases.rs

--- crates/parser/tests/main.rs
+++ crates/parser/tests/main.rs
@@ -1,31 +1,7 @@
 use parser::Parsed;
 use serde::{Deserialize, Serialize};
+#[allow(unused)]
 use weapon::{AttackSpeedModifier, DamageType, Explicit, FlatDamage, PhysModifier, Quality, Range};
-
-pub const INPUT_1: &str = "Item Class: Two Hand Maces
-Rarity: Rare
-Plague Crusher
-Leaden Greathammer
---------
-Quality: +20% (augmented)
-Physical Damage: 144-194 (augmented)
-Fire Damage: 7-16 (augmented)
-Critical Hit Chance: 5.00%
-Attacks per Second: 1.20 (augmented)
---------
-Requirements:
-Level: 33
-Str: 76
---------
-Item Level: 33
---------
-107% increased Physical Damage
-Adds 7 to 16 Fire Damage
-+88 to Accuracy Rating
-+16% to Critical Damage Bonus
-9% increased Attack Speed
-10% increased Light Radius
-";
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Fixture {
--- scrape/src/main.ts
+++ scrape/src/main.ts
@@ -1,18 +1,27 @@
 import playwright from 'playwright';
-import { scrap_page, WEAPON_TYPES } from './scrap_page';
+import node_fs_promises from 'node:fs/promises';
+import node_path from 'node:path';
+
+import { scrap_page, WEAPON_TYPES } from './scrap_page.ts';
 
 async function main() {
-	const browser = await playwright.chromium.launch({ timeout: 30000, headless: true });
+	const browser = await playwright.chromium.launch({ timeout: 30000, headless: false });
 	const context = await browser.newContext();
-
 	const promises = WEAPON_TYPES.map(async weapon_type => {
 		const page = await context.newPage();
 		return await scrap_page(weapon_type, page);
 	});
-	const results = await Promise.all(promises);
+	const results = (await Promise.all(promises)).flat();
+
+	let rs_str = 'pub const BASES: &str = [\n';
+	results.forEach(({ base }) => (rs_str += `\t"${base}",\n`));
+	rs_str += '];';
 
 	await browser.close();
-	await Bun.write('bases.json', JSON.stringify(results));
+	await node_fs_promises.writeFile(
+		node_path.resolve(import.meta.dirname, '../../crates/parser/src/bases.rs'),
+		rs_str
+	);
 }
 
 main();
