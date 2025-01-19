3ea76af932b181412941b6bab1c75abe37f3e430
shonya3 <poeshonya3@gmail.com>
2025-01-19 17:24:57 +0300

    create function for writing bases

--- scrape/src/main.ts
+++ scrape/src/main.ts
@@ -3,6 +3,7 @@ import node_fs_promises from 'node:fs/promises';
 import node_path from 'node:path';
 
 import { scrap_page, WEAPON_TYPES } from './scrap_page.ts';
+import type { WeaponStats } from './weapon.ts';
 
 async function main() {
 	const browser = await playwright.chromium.launch({ timeout: 30000, headless: true });
@@ -12,12 +13,15 @@ async function main() {
 		return await scrap_page(weapon_type, page);
 	});
 	const results = (await Promise.all(promises)).flat();
-
-	let rs_str = `pub const BASES: [&str; ${results.length}] = [\n`;
-	results.forEach(({ base }) => (rs_str += `\t"${base}",\n`));
-	rs_str += '];\n';
-
 	await browser.close();
+
+	await write_bases_rs_into_parser_crate(results);
+}
+
+async function write_bases_rs_into_parser_crate(weapons: Array<WeaponStats>): Promise<void> {
+	let rs_str = `pub const BASES: [&str; ${weapons.length}] = [\n`;
+	weapons.forEach(({ base }) => (rs_str += `\t"${base}",\n`));
+	rs_str += '];\n';
 	await node_fs_promises.writeFile(
 		node_path.resolve(import.meta.dirname, '../../crates/parser/src/bases.rs'),
 		rs_str
