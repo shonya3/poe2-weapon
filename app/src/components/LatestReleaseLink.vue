161bf929503bfcdbe35c6908885a38ea874bfa7d
shonya3 <poeshonya3@gmail.com>
2025-01-24 19:16:38 +0300

    fix: Fix bottom-right release link

--- app/src/components/LatestReleaseLink.vue
+++ app/src/components/LatestReleaseLink.vue
@@ -2,10 +2,11 @@
 import BrowserLink from './BrowserLink.vue';
 
 const version = import.meta.env.VERSION;
-const tag = `v${version}`;
+const tag = `app-v${version}`;
+const label = `v${version}`;
 const href = `https://github.com/shonya3/poe2-weapon/releases/tag/${tag}`;
 </script>
 
 <template>
-	<BrowserLink class="underline font-medium text-blue-600 dark:text-blue-500" :href="href">{{ tag }}</BrowserLink>
+	<BrowserLink class="underline font-medium text-blue-600 dark:text-blue-500" :href="href">{{ label }}</BrowserLink>
 </template>
