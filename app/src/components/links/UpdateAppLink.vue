2b2879c12da3b8302d6da2d3bba793a9b0c4b1ea
shonya3 <poeshonya3@gmail.com>
2025-01-25 12:24:44 +0300

    uncomment real code

--- app/src/components/links/UpdateAppLink.vue
+++ app/src/components/links/UpdateAppLink.vue
@@ -5,18 +5,14 @@ import { Link } from './Link';
 
 const { update } = use_tauri_updater();
 
-// const is_visible = computed(() => Boolean(update.value?.available));
-// const version = computed(() => update.value?.version);
-// const to = computed(() => `/update/${version.value}`);
-const is_visible = true;
-const version = '0.1.0';
-const to = `/update/${version}`;
+const is_visible = computed(() => Boolean(update.value?.available));
+const version = computed(() => update.value?.version);
+const to = computed(() => `/update/${version.value}`);
+// const is_visible = true;
+// const version = '0.1.0';
+// const to = `/update/${version}`;
 </script>
 
 <template>
-	<!-- <RouterLink v-if="is_visible" :to="to" class="underline font-medium text-blue-600 dark:text-blue-500">
-		Update is ready
-	</RouterLink> -->
-
 	<Link v-if="is_visible" :to="to" kind="app">Update is Ready</Link>
 </template>
