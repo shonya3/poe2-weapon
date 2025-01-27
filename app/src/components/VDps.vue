b2b6aeea1b18d8fea0eb50bfa5c99dcd0c35fe7b
shonya3 <poeshonya3@gmail.com>
2025-01-27 18:11:35 +0300

    feat: add tooltip with DPS breakdown and themed icons

--- app/src/components/VDps.vue
+++ app/src/components/VDps.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { fmt } from '../formatter';
 import { Dps } from '../types';
+import '../elements/simple-tooltip';
 defineProps<{ dps: Dps }>();
 </script>
 
 <template>
-	<div>{{ fmt(dps.total) }}</div>
+	<div>
+		<div>{{ fmt(dps.total) }}</div>
+		<simple-tooltip>
+			<div class="space-y-2 p-4 bg-white border border-gray-200 rounded-lg shadow-lg text-base">
+				<div v-if="dps.pdps" class="text-amber-900">
+					⚙️ Physical: <span>{{ fmt(dps.pdps) }}</span>
+				</div>
+				<div v-if="dps.edps" class="text-sky-600">🌊 Elemental: {{ fmt(dps.edps) }}</div>
+				<div v-if="dps.cdps" class="text-purple-800">🌀 Chaos: {{ fmt(dps.cdps) }}</div>
+			</div>
+		</simple-tooltip>
+	</div>
 </template>
