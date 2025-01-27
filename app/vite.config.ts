64f2827edabfdad367d08013c2e7831eaa6b9f8c
shonya3 <poeshonya3@gmail.com>
2025-01-27 07:15:54 +0300

    add vue devtools

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,13 +3,14 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import { version } from './package.json';
 import vue_jsx from '@vitejs/plugin-vue-jsx';
+import vue_devtools from 'vite-plugin-vue-devtools';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [vue(), vue_jsx(), tailwindcss()],
+	plugins: [vue(), vue_jsx(), vue_devtools(), tailwindcss()],
 
 	define: {
 		'import.meta.env.VERSION': JSON.stringify(version),
