80850be868e7c96902e66fdbfc6c14ca46081668
shonya3 <poeshonya3@gmail.com>
2025-01-30 17:48:51 +0300

    remove dead code

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import HomeView from './views/HomeView.vue';
 import ClipboardFlowView from './views/ClipboardFlowView.vue';
 import UpdateView from './views/UpdateView.vue';
 import TrayWindow from './views/TrayWindow.vue';
 
 const routes = [
-	{ path: '/', component: HomeView },
 	{ path: '/clipboard-flow', component: ClipboardFlowView },
 	{ path: '/update/:new_version', component: UpdateView },
 	{ path: '/tray-window', component: TrayWindow },
