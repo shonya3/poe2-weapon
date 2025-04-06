d0f104e06abce71aed7ecc18a65ad24af2d7420a
shonya3 <poeshonya3@gmail.com>
2025-04-06 13:17:18 +0300

    Add tooltip for Rune

--- app/src/components/VRune.vue
+++ app/src/components/VRune.vue
@@ -8,10 +8,17 @@ const { size = 40, show_runes_names = false } = defineProps<{
 </script>
 
 <template>
-	<div class="flex flex-col items-center">
-		<img :width="size" :height="size" :src="`/runes/${variant}.webp`" :alt="variant" />
-		<div v-if="show_runes_names" class="text-sm font-semibold">
-			{{ variant.replace('Greater', 'G. ').replace('Lesser', 'L. ') }}
+	<div>
+		<div class="flex flex-col items-center">
+			<img :width="size" :height="size" :src="`/runes/${variant}.webp`" :alt="variant" />
+			<div v-if="show_runes_names" class="text-sm font-semibold">
+				{{ variant.replace('Greater', 'G. ').replace('Lesser', 'L. ') }}
+			</div>
 		</div>
+		<simple-tooltip>
+			<div class="space-y-2 p-4 bg-white border border-gray-200 rounded-lg shadow-lg text-base">
+				{{ variant.replace('Lesser', 'Lesser ').replace('Greater', 'Greater ') }}
+			</div>
+		</simple-tooltip>
 	</div>
 </template>
